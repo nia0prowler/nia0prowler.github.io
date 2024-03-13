2e1f751f526b68c126e2526d0e03a98d154d8432
Igor Nosov <igor.nosov@rtits.ru>
2024-03-13 13:23:33 +0300

    new pwa project

---
+++ precache-manifest.55f1c1574bf9a85a990b33e1788b962c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef300b6d3a31dd9714ab564b1271d10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "825d227be74e5c81ede0",
+    "url": "/static/css/main.e541d4f3.chunk.css"
+  },
+  {
+    "revision": "7d93d186fe2ecad4adbe",
+    "url": "/static/js/2.f5534a0f.chunk.js"
+  },
+  {
+    "revision": "1876019e6f8cf7ca83e2",
+    "url": "/static/js/3.0e4cdb98.chunk.js"
+  },
+  {
+    "revision": "825d227be74e5c81ede0",
+    "url": "/static/js/main.a0708a5b.chunk.js"
+  },
+  {
+    "revision": "08358a26ce333fe80872",
+    "url": "/static/js/runtime~main.b8f3c4c4.js"
+  }
+]);
